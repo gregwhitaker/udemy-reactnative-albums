318f0e07ed7865b6e11fcb9edc73fdcb2c352be7
gregwhitaker <gwhitake@gmail.com>
2017-04-08 16:39:58 -0700

    Imported react and react-native

---
+++ index.ios.js
@@ -0,0 +1,9 @@
+// Place iOS code in here
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+
+// Render it to the device
