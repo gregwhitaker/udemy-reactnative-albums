718c6a9692a4e666318253a7579eb3f9413755d7
gregwhitaker <gwhitake@gmail.com>
2017-04-08 17:36:46 -0700

    Adding view to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,23 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-    const { textStyle } = styles;
+    const { textStyle, viewStyle } = styles;
 
     return (
-        <Text style={textStyle}>Albums</Text>
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
     );
 };
 
 const styles = {
-    textStyle : {
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
+    textStyle: {
         fontSize: 20
     }
 };
