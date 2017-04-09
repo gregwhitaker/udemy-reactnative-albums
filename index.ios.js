93047f1a0ffd6ffe6f49c90d259f123dc23a5daa
gregwhitaker <gwhitake@gmail.com>
2017-04-08 17:00:33 -0700

    Got some text on the screen

--- index.ios.js
+++ index.ios.js
@@ -2,8 +2,14 @@
 
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 // Create a component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
 
 // Render it to the device
+AppRegistry.registerComponent('albums', () => App);
